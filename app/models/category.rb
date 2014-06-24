281607e6c3e284803de84c1ef095d1b5a2055ebf
Jacob Coble <c4bee386@opayq.com>
2014-06-24 12:10:22 -0400

    Correct wrong name on Category association.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   validates :name, :region_id, presence: true
   
-  belongs_to :parent_region, class_name: "Category", foreign_key: :parent_id
+  belongs_to :parent_category, class_name: "Category", foreign_key: :parent_id
   has_many :children, -> (parent) { where region_id: parent.region_id },
     class_name: "Category", foreign_key: :parent_id
 end
