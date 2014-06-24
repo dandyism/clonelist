20c7fd57a23e62efb0674db13d538a565ec7dfb9
Jacob Coble <c4bee386@opayq.com>
2014-06-24 10:52:51 -0400

    Add an index and presence constraints.

--- db/migrate/20140624144950_create_regions.rb
+++ db/migrate/20140624144950_create_regions.rb
@@ -2,9 +2,11 @@ class CreateRegions < ActiveRecord::Migration
   def change
     create_table :regions do |t|
       t.integer :parent_id
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
+    
+    add_index :regions, :parent_id
   end
 end
