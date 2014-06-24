8adc9ed58d936f2a06e50be34f81dfd218d716e8
Jacob Coble <c4bee386@opayq.com>
2014-06-24 11:25:59 -0400

    Add parent and child associations for the Category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
   validates :name, :region_id, presence: true
+  
+  belongs_to :parent, class_name: "Category", foreign_key: :parent_id
+  has_many :children, class_name: "Category", foreign_key: :parent_id
 end
