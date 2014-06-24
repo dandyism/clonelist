761a317548b28282a52fff9d53712388131e0a59
Jacob Coble <c4bee386@opayq.com>
2014-06-24 11:21:21 -0400

    Add indicies and presence constraints to categories.

--- db/migrate/20140624151822_create_categories.rb
+++ db/migrate/20140624151822_create_categories.rb
@@ -2,10 +2,13 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.integer :parent_id
-      t.string :name
-      t.integer :region_id
+      t.string :name, null: false
+      t.integer :region_id, null: false
 
       t.timestamps
     end
+    
+    add_index :categories, :region_id
+    add_index :categories, :parent_id
   end
 end
