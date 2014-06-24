f80a0eb24bcc6114af68f6e10ef6dbb1d19d245a
Jacob Coble <c4bee386@opayq.com>
2014-06-24 15:37:32 -0400

    Migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624183947) do
+ActiveRecord::Schema.define(version: 20140624191958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ads", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "location"
+    t.integer  "region_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ads", ["region_id", "category_id"], name: "index_ads_on_region_id_and_category_id", using: :btree
 
   create_table "categories", force: true do |t|
     t.integer  "parent_id"
@@ -24,6 +37,15 @@ ActiveRecord::Schema.define(version: 20140624183947) do
   end
 
   add_index "categories", ["parent_id"], name: "index_categories_on_parent_id", using: :btree
+
+  create_table "category_hierarchies", force: true do |t|
+    t.integer "ancestor_id",   null: false
+    t.integer "descendant_id", null: false
+    t.integer "generations",   null: false
+  end
+
+  add_index "category_hierarchies", ["ancestor_id", "descendant_id", "generations"], name: "category_anc_desc_idx", unique: true, using: :btree
+  add_index "category_hierarchies", ["descendant_id"], name: "category_desc_idx", using: :btree
 
   create_table "region_hierarchies", force: true do |t|
     t.integer "ancestor_id",   null: false
