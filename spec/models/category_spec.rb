513cad0ed373bc9e30ac3173bf3fd8ad5aa924db
Jacob Coble <c4bee386@opayq.com>
2014-06-24 12:07:36 -0400

    Add a test for region_id assignment in Category associations.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should give child categories the same region id" do
+    category = Category.new(name: "for sale", region_id: 1)
+    subcategory = category.children.new(name: "appliances")
+    expect(subcategory.region_id).to eq category.region_id
+  end
+  
 end
