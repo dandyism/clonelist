c435142000e849a2d47da1b9258795cca198accf
Jacob Coble <c4bee386@opayq.com>
2014-06-24 10:58:21 -0400

    Add region parent and child associations.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,6 @@
 class Region < ActiveRecord::Base
   validates :name, presence: true
+  
+  belongs_to :parent_regions, class_name: "Region", foreign_key: :parent_id
+  has_many :child_regions, class_name: "Region", foreign_key: :parent_id
 end
