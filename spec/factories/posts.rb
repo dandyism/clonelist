06d71d2e667deed3309eba2bcda8943cb6a64383
Jacob Coble <c4bee386@opayq.com>
2014-06-25 12:19:18 -0400

    Fix bug where factories would generate indentical values each time

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :post do
-    title Faker::Commerce.product_name
-    description Faker::Lorem.paragraph
-    author_id 1
-    price Faker::Commerce.price
-    location { "#{Faker::Address.city}, #{Faker::Address.state_abbr}"}
+    title { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    association :author, factory: :user
+    price { Faker::Commerce.price }
+    location { "#{Faker::Address.city}, #{Faker::Address.state_abbr}" }
     category
   end
 end
