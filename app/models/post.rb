b7f9a93937c3bf9aed1e2e8cdcf596a441e181e8
Jacob Coble <c4bee386@opayq.com>
2014-06-25 08:46:37 -0400

    Fix assocation between users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, :description, :author_id, presence: true
   validates :price, numericality: true
   
-  belongs_to :user
+  belongs_to :author, class_name: "User", foreign_key: :author_id
 end
