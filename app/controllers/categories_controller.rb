abaa5f53c3ce9cd664f1d6d699a4bb5d31adcc12
Jacob Coble <c4bee386@opayq.com>
2014-06-25 15:03:11 -0400

    Make category show page filter posts if search query present

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,5 +6,16 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
+
+    if params[:search]
+      keywords = params[:search][:keywords]
+      query = ""
+
+      keywords.each do |keyword|
+        query += "title LIKE '%#{keyword}% OR description LIKE '%#{keyword}%' "
+      end
+
+      @posts = @category.posts.where(query)
+    end
   end
 end
