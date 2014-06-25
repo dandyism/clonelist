be536fdb067d3193b4f67157d155a7fad2fd192e
Jacob Coble <c4bee386@opayq.com>
2014-06-24 23:11:09 -0400

    Test invalid Post price

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe Post, :type => :model do
   it "should require an author" do
     expect(FactoryGirl.build(:post, author_id: nil)).not_to be_valid
   end
+  
+  it "should require that price is a decimal" do
+    expect(FactoryGirl.build(:post, price: "string")).not_to be_valid
+  end
 end
