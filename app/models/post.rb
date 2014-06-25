b6d9ed0633f10177e947a02014cfb4d6d2461e1f
Jacob Coble <c4bee386@opayq.com>
2014-06-25 10:19:27 -0400

    Validate presence of category in Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, :description, :author_id, presence: true
+  validates :title, :description, :author_id, :category, presence: true
   validates :price, numericality: true
   
   belongs_to :author, class_name: "User", foreign_key: :author_id
