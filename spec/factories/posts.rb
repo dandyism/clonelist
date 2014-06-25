83c7e6be12dc46ee6848579a4a0d4bd4ac43b5cd
Jacob Coble <c4bee386@opayq.com>
2014-06-24 22:13:05 -0400

    Add price and location to Post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     title Faker::Commerce.product_name
     description Faker::Lorem.paragraph
     author_id 1
+    price Faker::Commerce.price
+    location { "#{Faker::Address.city}, #{Faker::Address.state_abbr}"}
   end
 end
