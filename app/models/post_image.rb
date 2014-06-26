a9c7ade4678aaf82cff1adc0428c081dc5332789
Jacob Coble <c4bee386@opayq.com>
2014-06-26 10:37:47 -0400

    Add attachment content validator to PostImage

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,6 @@
 class PostImage < ActiveRecord::Base
   has_attached_file :file
+  validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   belongs_to :post
 
