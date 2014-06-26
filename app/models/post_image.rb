73e7aa4dddf759b39539e3c75163e27931033e82
Jacob Coble <c4bee386@opayq.com>
2014-06-26 14:40:49 -0400

    Validate presence of attachments for PostImage

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,6 @@
 class PostImage < ActiveRecord::Base
   has_attached_file :file
-  validates_attachment :file,
+  validates_attachment :file, presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
     size: { in: 0..204.kilobytes }
 
