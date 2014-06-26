c490b4c887dcb050f00282a8f8ab0df0d6de3085
Jacob Coble <c4bee386@opayq.com>
2014-06-26 08:35:47 -0400

    Add test for category name uniqueness constraint

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Category, :type => :model do
   it "should require a name" do
     expect(FactoryGirl.build(:category, name: nil)).not_to be_valid
   end
+
+  it "should enforce name uniqueness" do
+    FactoryGirl.create(:category, name: "Duplicate")
+    expect(FactoryGirl.build(:category, name: "Duplicate")).not_to be_valid
+  end
 end
