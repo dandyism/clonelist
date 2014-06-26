a7d9205bfa5f684ef5499aaea22962fd69fb7cf6
Jacob Coble <c4bee386@opayq.com>
2014-06-26 14:38:33 -0400

    Add tests for PostImage validations

--- spec/models/post_image_spec.rb
+++ spec/models/post_image_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PostImage, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have an attached file" do
+    should have_attached_file(:file)
+  end
+
+  it "should validate attachment presence" do
+    should validate_attachment_presence(:file)
+  end
+
+  it "should validate attachment type" do
+    should validate_attachment_content_type(:file)
+      .allowing('image/png', 'image/gif')
+      .rejecting('text/plain', 'text/xml')
+  end
+
+  it "should validate attachment size" do
+    should validate_attachment_size(:file)
+      .in(0..204.kilobytes)
+  end
 end
