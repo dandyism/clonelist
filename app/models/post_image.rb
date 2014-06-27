6cb73bbf8c33673d49b3d2ea81b6d99067a3a39b
Jacob Coble <c4bee386@opayq.com>
2014-06-27 15:50:08 -0400

    Fix thumbnail bug

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,10 @@
 class PostImage < ActiveRecord::Base
-  has_attached_file :file
+  has_attached_file :file,
+    styles: { thumbnail: "50x50>" }
+
   validates_attachment :file, presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-    size: { in: 0..204.kilobytes },
-    styles: { thumbnail: "50x50>" }
+    size: { in: 0..204.kilobytes }
 
   belongs_to :post
 
