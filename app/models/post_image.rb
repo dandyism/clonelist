a3fce1ca9269124e04a1928721c3a93c1a9b0796
Jacob Coble <c4bee386@opayq.com>
2014-06-27 09:25:54 -0400

    Add image thumbnails

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,8 @@ class PostImage < ActiveRecord::Base
   has_attached_file :file
   validates_attachment :file, presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-    size: { in: 0..204.kilobytes }
+    size: { in: 0..204.kilobytes },
+    styles: { thumbnail: "50x50>" }
 
   belongs_to :post
 
