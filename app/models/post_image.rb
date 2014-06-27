8f21b6bccdbe95523c40be9c3364b8b51d478b32
Jacob Coble <c4bee386@opayq.com>
2014-06-27 15:55:31 -0400

    Scale down uploaded images

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,10 +1,10 @@
 class PostImage < ActiveRecord::Base
   has_attached_file :file,
-    styles: { thumbnail: "50x50>" }
+    styles: { thumbnail: "50x50>", original: "512x512>" }
 
   validates_attachment :file, presence: true,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-    size: { in: 0..204.kilobytes }
+    size: { less_than: 5.megabytes }
 
   belongs_to :post
 
