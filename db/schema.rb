0a38bf3810c4db112f554e8d4540655e1ce1027f
App Academy Student <student@appacademy.io>
2014-06-27 12:41:44 -0400

    Remove name column from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627143248) do
+ActiveRecord::Schema.define(version: 20140627164031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 20140627143248) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
