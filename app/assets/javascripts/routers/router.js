9896536c1948df4dd113ebc2b112d958b7f24489
Jacob Coble <c4bee386@opayq.com>
2014-06-29 19:10:02 -0400

    Add category show view to router

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,18 +1,40 @@
 Clonelist.Routers.Router = Backbone.Router.extend({
   initialize: function(options) {
     this.$rootEl = options.$rootEl;
+    this.categories = Clonelist.Collections.categories;
   },
 
   routes: {
-    '': 'index'
+    '': 'index',
+    'categories/:id': 'showCategory'
   },
 
   index: function() {
     var categoriesIndex = new Clonelist.Views.CategoriesIndex({
-      collection: Clonelist.Collections.categories
+      collection: this.categories
     });
 
     this._swapView(categoriesIndex);
+  },
+
+  showCategory: function(id) {
+    var router = this;
+    var category = this.categories.get(id);
+
+    if (!category) { 
+      category = new Clonelist.Models.Category({ id: id });
+      category.fetch({
+        success: function() {
+          router.categories.add(category);
+        }
+      });
+    }
+
+    var showCategory = new Clonelist.Views.CategoryShow({
+      model: category
+    });
+
+    this._swapView(showCategory);
   },
 
   _swapView: function(view) {
