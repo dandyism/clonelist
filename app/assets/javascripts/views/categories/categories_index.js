50ee952e2d4bd721c024824bd02c44eda58ebfa3
Jacob Coble <c4bee386@opayq.com>
2014-06-29 18:06:34 -0400

    Put missing comma in categories index

--- app/assets/javascripts/views/categories/categories_index.js
+++ app/assets/javascripts/views/categories/categories_index.js
@@ -2,12 +2,12 @@ Clonelist.Views.CategoriesIndex = Backbone.View.extend({
 
   initialize: function(options) {
     this.listenTo(this.collection, "sync", this.render);
-  }
+  },
 
   template: JST['categories/index'],
 
   render: function() {
-    var rendered = template({ categories: this.collection });
+    var rendered = this.template({ categories: this.collection });
     this.$el.html(rendered)
     return this;
   }
