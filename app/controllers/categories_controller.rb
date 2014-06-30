5c7ea72dc0343a4cd711e5d0b7c3a4effdbd06dd
App Academy Student <student@appacademy.io>
2014-06-30 11:54:15 -0400

    Move post listing into a single partial

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,36 +4,9 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
-  
+
   def show
-    conditions = []
-    values = []
-
-    unless params[:keywords].blank?
-      keywords = params[:keywords].split
-
-      keywords.each do |keyword|
-        conditions << <<-SQL
-          (UPPER(posts.title) LIKE UPPER(?) 
-          OR UPPER(posts.description) LIKE UPPER(?))
-        SQL
-
-        values += ["%#{keyword}%"] * 2 
-      end
-    end
-
-    unless params[:min_price].blank?
-      conditions << 'posts.price >= ?'
-      values << params[:min_price]
-    end
-
-    unless params[:max_price].blank?
-      conditions << 'posts.price <= ?'
-      values << params[:max_price]
-    end
-
-    query = conditions.join(' AND ')
     @category = Category.find(params[:id])
-    @posts = @category.posts.where(query, *values)
+    @posts = @category.posts.page(params[:page])
   end
 end
