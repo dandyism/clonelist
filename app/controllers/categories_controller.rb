566f0385c14e1c7b4b50fdfd234534f9a238b814
Jacob Coble <c4bee386@opayq.com>
2014-07-01 17:51:24 -0400

    Filter returned posts by keywords in category show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,5 +8,9 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @posts = @category.posts.page(params[:page])
+
+    if params[:keywords].present?
+      @posts = @posts.search_by_keywords(params[:keywords])
+    end
   end
 end
