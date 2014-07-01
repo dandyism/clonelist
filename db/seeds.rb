efb59cc508921ce1c5d15515def1db4c094835a6
Jacob Coble <c4bee386@opayq.com>
2014-07-01 09:24:21 -0400

    Create seed data for users and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,47 @@
 User.create(username: "guest", email: "guest@example.com", password: "guestpassword")
-User.create(username: "greg", email: "greg@example.com", password: "letmein")
-User.create(username: "nick", email: "nick@example.com", password: "letmein")
-User.create(username: "jim", email: "jim@example.com", password: "letmein")
+User.create(username: "4mepieter", email: "4mepieter@example.com", password: "letmein")
+User.create(username: "beastrain", email: "beastrain@example.com", password: "letmein")
+User.create(username: "brailnet", email: "brailnet@example.com", password: "letmein")
+User.create(username: "burntso", email: "burntso@example.com", password: "letmein")
+User.create(username: "chronosfire", email: "chronosfire@example.com", password: "letmein")
+User.create(username: "cooladictu", email: "cooladictu@example.com", password: "letmein")
+User.create(username: "corabat", email: "corabat@example.com", password: "letmein")
+User.create(username: "dareramb", email: "dareramb@example.com", password: "letmein")
+User.create(username: "doodlerybo", email: "doodlerybo@example.com", password: "letmein")
+User.create(username: "eplamide", email: "eplamide@example.com", password: "letmein")
+User.create(username: "exotictith", email: "exotictith@example.com", password: "letmein")
+User.create(username: "fatevintage", email: "fatevintage@example.com", password: "letmein")
+User.create(username: "forlifecu", email: "forlifecu@example.com", password: "letmein")
+User.create(username: "garrica", email: "garrica@example.com", password: "letmein")
+User.create(username: "henryolpist", email: "henryolpist@example.com", password: "letmein")
+User.create(username: "inestarp", email: "inestarp@example.com", password: "letmein")
+User.create(username: "iwanterco", email: "iwanterco@example.com", password: "letmein")
+User.create(username: "japatels", email: "japatels@example.com", password: "letmein")
+User.create(username: "kroolormace", email: "kroolormace@example.com", password: "letmein")
+User.create(username: "mabelluti", email: "mabelluti@example.com", password: "letmein")
+User.create(username: "mincorett", email: "mincorett@example.com", password: "letmein")
+User.create(username: "nightwest", email: "nightwest@example.com", password: "letmein")
+User.create(username: "nulomber", email: "nulomber@example.com", password: "letmein")
+User.create(username: "phoenixonli", email: "phoenixonli@example.com", password: "letmein")
+User.create(username: "rhombland", email: "rhombland@example.com", password: "letmein")
+User.create(username: "ridaseauer", email: "ridaseauer@example.com", password: "letmein")
+User.create(username: "sarensemi", email: "sarensemi@example.com", password: "letmein")
+User.create(username: "shimagen", email: "shimagen@example.com", password: "letmein")
+User.create(username: "spellco", email: "spellco@example.com", password: "letmein")
 
-Category.create(name: "Housewares")
-Category.create(name: "Food")
-Category.create(name: "Toys")
-Category.create(name: "Clothing")
-Category.create(name: "Military Surplus")
+Category.create(name: "Antiques");
+Category.create(name: "Appliances");
+Category.create(name: "Automotive");
+Category.create(name: "Books");
+Category.create(name: "Clothing");
+Category.create(name: "Collectibles");
+Category.create(name: "Computer");
+Category.create(name: "Electronics");
+Category.create(name: "Furniture");
+Category.create(name: "General");
+Category.create(name: "Household");
+Category.create(name: "Jewelry");
+Category.create(name: "Sporting");
+Category.create(name: "Tools");
+Category.create(name: "Toys and Games");
+Category.create(name: "Video Gaming");
