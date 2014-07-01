8c6e319ecffbd019593c999c07f5f70ce2419f78
Jacob Coble <c4bee386@opayq.com>
2014-07-01 17:46:30 -0400

    Add input event listener to search input

--- app/assets/javascripts/views/categories/categories_show.js
+++ app/assets/javascripts/views/categories/categories_show.js
@@ -1,11 +1,18 @@
 Clonelist.Views.CategoryShow = Backbone.View.extend({
 
+  template: JST["categories/show"],
+
   initialize: function(options) {
     this.listenTo(this.model, "sync", this.render);
     this.listenTo(this.model.posts(), "sync add", this.render);
   },
 
-  template: JST["categories/show"],
+  events: {
+    "input #search-keywords": "filter"
+  },
+
+  filter: function(event) {
+  },
 
   render: function() {
     var rendered = this.template({ category: this.model });
