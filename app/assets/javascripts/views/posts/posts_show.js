8fa8ace3b06bec9444ca5c1af7aa698cdd62a40f
Jacob Coble <c4bee386@opayq.com>
2014-07-02 12:13:53 -0400

    Add images to post show view

--- app/assets/javascripts/views/posts/posts_show.js
+++ app/assets/javascripts/views/posts/posts_show.js
@@ -7,7 +7,11 @@ Clonelist.Views.PostShow = Backbone.View.extend({
   template: JST['posts/show'],
 
   render: function() {
-    var rendered = this.template({ post: this.model });
+    var rendered = this.template({
+      post: this.model,
+      images: this.model.get('image_urls')
+    });
+
     this.$el.html(rendered);
     this.trigger("render");
     return this;
